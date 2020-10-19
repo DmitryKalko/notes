d72c683d5378c6daeb5ed1b428fae98352586297
Dmitry Kalko <master-denn@mail.ru>
2020-10-19 20:06:06 +0300

    supplemented task object, add styles to new task

--- src/functional.js
+++ src/functional.js
@@ -5,18 +5,23 @@ const addWindow = document.querySelector('.icon-plus');
 const closeWindow = document.querySelector('.icon-cross');
 const leftMenu = document.querySelector('.left-menu');
 const mainBlockInputField = document.querySelector('.main-block_input-field');
-//const workZone = document.querySelector('.work-zone');
 const todoList = document.querySelector('.todo-list');
 
+
 //--- появление / исчезновение левого меню ---
-document.onmouseover = function(e) {
-    leftMenu.classList.remove("left-menu");
-    leftMenu.classList.add("left-menu-on");
+document.onmouseover = onmouseOver;
+function onmouseOver(e) {
+    leftMenu.classList.remove('left-menu');
+    leftMenu.classList.add('left-menu-on');
+    newTask.classList.remove('full-space');
   };
-  document.onmouseout = function(e) {
-    leftMenu.classList.remove("left-menu-on");
-    leftMenu.classList.add("left-menu");
+document.onmouseout = mouseOut;
+function mouseOut(e) {
+    leftMenu.classList.remove('left-menu-on');
+    leftMenu.classList.add('left-menu');
+    newTask.classList.add('full-space');
   };
+
 
 //--- открытие нового окна ---
   addWindow.onclick = creatingNewWindow;
@@ -50,7 +55,12 @@ function submitForm(e) {
     e.preventDefault();
 
     let task = {
+        id: 0,
         text: formInput.value,
+        doneStatus: false,
+        redStatus: false,
+        yellowStatus: false,
+        greenStatus: false,
     }
 
     tasks.push(task);
@@ -61,24 +71,27 @@ function submitForm(e) {
 //--- внешний вид и функционал задачи ---
 function createTasksBlock(task, index) {
     const newTask = document.createElement('li');
-    newTask.classList.add('newtTask');
+    newTask.classList.add('newTask');
     const taskText = document.createElement('span');
     taskText.classList.add('taskText');
+    const icons = document.createElement('div');
+    icons.classList.add('icons');
     const doneTask = document.createElement('button');
-    doneTask.classList.add('doneTask');
+    doneTask.classList.add('icon-checkmark');
 
     //doneTask.onclick = 
 
     const deleteTask = document.createElement('button');
-    deleteTask.classList.add('deleteTask');
+    deleteTask.classList.add('icon-bin');
 
     deleteTask.onclick = function() {
 		tasks.splice(index, 1);
 		renderApp();
-	}
+    }
 
     taskText.textContent = task.text;
-    newTask.append(taskText, doneTask, deleteTask);
+    icons.append(doneTask, deleteTask)
+    newTask.append(taskText, icons);
 
     return newTask;
 }
@@ -92,3 +105,5 @@ function renderApp() {
 		todoList.append(tasksBlock);
     }
 }
+
+
