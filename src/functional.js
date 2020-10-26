3284c8cf0916b18266e0ffcaa149f5459cd99845
Dmitry Kalko <master-denn@mail.ru>
2020-10-26 20:56:34 +0300

    add active task, fixed coloring tasks

--- src/functional.js
+++ src/functional.js
@@ -17,37 +17,37 @@ const todoList = document.querySelector('.todo-list');
 //--- появление / исчезновение левого меню ---
 document.onmouseover = onmouseOver;
 function onmouseOver(e) {
-    leftMenu.classList.remove('left-menu');
-    leftMenu.classList.add('left-menu-on');
+  leftMenu.classList.remove('left-menu');
+  leftMenu.classList.add('left-menu-on');
 };
 document.onmouseout = mouseOut;
 function mouseOut(e) {
-    leftMenu.classList.remove('left-menu-on');
-    leftMenu.classList.add('left-menu');
+  leftMenu.classList.remove('left-menu-on');
+  leftMenu.classList.add('left-menu');
 };
 
 
 //--- открытие нового окна ---
 addWindow.onclick = creatingNewWindow;
 function creatingNewWindow() {
-    ipcRenderer.send('open');
+  ipcRenderer.send('open');
 }
 
 //--- закрытие окна ---
 closeWindow.onclick = closingWindow;
 function closingWindow() {
-    Swal.fire({
-        title: 'Точно закрыть это окно?',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Закрыть',
-        cancelButtonText: 'Нет',
-    }).then((result) => {
-        if (result.isConfirmed) {
-            remote.getCurrentWindow().close();
-        }
-    })
+  Swal.fire({
+    title: 'Точно закрыть это окно?',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Закрыть',
+    cancelButtonText: 'Нет',
+  }).then((result) => {
+    if (result.isConfirmed) {
+      remote.getCurrentWindow().close();
+    }
+  })
 }
 
 //--- разметка формы добавления задач ---
@@ -65,228 +65,258 @@ addForm.append(formInput);
 
 addForm.onsubmit = submitForm;
 function submitForm(e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    let task = {
-        id: null,
-        text: formInput.value,
-        doneStatus: false,
-        redStatus: false,
-        yellowStatus: false,
-        greenStatus: false,
-    }
+  let task = {
+    id: null,
+    text: formInput.value,
+    doneStatus: false,
+    redStatus: false,
+    yellowStatus: false,
+    greenStatus: false,
+    activeStatus: false,
+  }
 
-    task.id = uniqId
-    uniqId++;
-    tasks.push(task);
-    addForm.reset();
-    renderApp();
-    console.log(tasks);
-    console.log(activeTask);
+  task.id = uniqId
+  uniqId++;
+  tasks.push(task);
+  addForm.reset();
+  tasks = tasks.map(task => {
+    task.activeStatus = false;
+    return task;
+  })
+    
+  renderApp();
+  console.log(tasks);
+  console.log(activeTask);
 }
 
 //--- ВНЕШНИЙ ВИД И ФУНКЦИОНАЛ ЗАДАЧИ ---
 function createTasksBlock(task) {
-    const newTask = document.createElement('li');
-    newTask.classList.add('newTask');
-    newTask.id = task.id.toString();
-    const taskText = document.createElement('span');
-    taskText.classList.add('taskText');
-    const icons = document.createElement('div');
-    icons.classList.add('icons');
-    const doneTask = document.createElement('button');
-    doneTask.classList.add('icon-checkmark');
-    const deleteTask = document.createElement('button');
-    deleteTask.classList.add('icon-bin');
+  const newTask = document.createElement('li');
+  newTask.classList.add('newTask');
+  const taskText = document.createElement('span');
+  taskText.classList.add('taskText');
+  const icons = document.createElement('div');
+  icons.classList.add('icons');
+  const doneTask = document.createElement('button');
+  doneTask.classList.add('icon-checkmark');
+  const deleteTask = document.createElement('button');
+  deleteTask.classList.add('icon-bin');
 
-    //--- отметить задачу как выполненную ---
-    doneTask.onclick = (e) => {
-        e.stopPropagation();
-        makeTaskDone(task);
+  //--- отметить задачу как выполненную ---
+  doneTask.onclick = (e) => {
+    e.stopPropagation();
+    makeTaskDone(task);
+  }
+
+  //--- активная задача / выделение задачи цветом ---
+  newTask.onclick = () => {
+    makeTaskActive(task);
+    highlightingTask(task);
+  }
+    
+  //проверка статуса активности      
+  checkActiveStatus(task);
+  function checkActiveStatus(task) {
+    if (task.activeStatus === true) {
+      newTask.classList.add('active');
+    } else {
+      newTask.classList.remove('active');
     }
+  }
 
-    //--- активная задача / выделение задачи цветом ---
-    newTask.onclick = () => {
-        highlightingTask(task);
+  // проверка цветовых статусов
+  checkColorStatuses(task);
+  function checkColorStatuses(task) {
+    if (task.redStatus === true) {
+      newTask.classList.add('make-red');
+    } else if (task.yellowStatus === true) {
+      newTask.classList.add('make-yellow');
+    } else if (task.greenStatus === true) {
+      newTask.classList.add('make-green');
+    } else {
+      newTask.classList.remove('make-red');
+      newTask.classList.remove('make-yellow');
+      newTask.classList.remove('make-green');
     }
+  }
 
-    // проверка цветовых статусов
-    checkColorStatuses(task);
-    function checkColorStatuses(task) {
-        if (task.redStatus === true) {
-            console.log(task.redStatus)
-            console.log(task)
-            newTask.classList.add('make-red');
-        } else if (task.yellowStatus === true) {
-            newTask.classList.add('make-yellow');
-        } else if (task.greenStatus === true) {
-            newTask.classList.add('make-green');
-        } else {
-            newTask.classList.remove('make-red');
-            newTask.classList.remove('make-yellow');
-            newTask.classList.remove('make-green');
-        }
+  //проверка статуса выполнения
+  checkDoneStatus(task);
+  function checkDoneStatus(task) {
+    if (task.doneStatus === true) {
+      taskText.classList.add('text-done');
+      newTask.classList.add('task-done');
+    } else {
+      taskText.classList.remove('text-done');
+      newTask.classList.remove('task-done');
     }
+  }
 
-    //проверка статуса выполнения
-    checkDoneStatus(task);
-    function checkDoneStatus(task) {
-        if (task.doneStatus === true) {
-            taskText.classList.add('text-done');
-            newTask.classList.add('task-done');
-        } else {
-            taskText.classList.remove('text-done');
-            newTask.classList.remove('task-done');
-        }
-    }
+  //--- удаление задачи ---
+  deleteTask.onclick = (e) => {
+    e.stopPropagation();
+    deleteOneTask(task)
+  };
 
-    //--- удаление задачи ---
-    deleteTask.onclick = (e) => {
-        e.stopPropagation();
-        deleteOneTask(task)
-    };
-
-    taskText.textContent = task.text;
-    icons.append(doneTask, deleteTask)
-    newTask.append(taskText, icons);
-    return newTask;
+  taskText.textContent = task.text;
+  icons.append(doneTask, deleteTask)
+  newTask.append(taskText, icons);
+  return newTask;
 }
 
 //--- перерисовка списка задач ---
 function renderApp() {
-    todoList.innerHTML = '';
-    for (let i = 0; i < tasks.length; i++) {
-        let tasksList = createTasksBlock(tasks[i]);
-        todoList.append(tasksList);
-        console.log('Render отработал')
-    }
+  todoList.innerHTML = '';
+  for (let i = 0; i < tasks.length; i++) {
+    let tasksList = createTasksBlock(tasks[i]);
+    todoList.append(tasksList);
+    console.log('Render отработал')
+  }
+  activeTask = null;
 }
 
-function makeTaskDone(task) {
-    if (task.doneStatus === false) {
-        task.doneStatus = true;
-    }
-    else {
-        task.doneStatus = false;
-    }
-    renderApp();
+function makeTaskDone(task) {      
+  if (task.doneStatus === false) {
+    task.doneStatus = true;
+  }
+  else {
+    task.doneStatus = false;
+  }
+  renderApp();
 }
 
-function highlightingTask(task) {   // ДОРАБОТАТЬ!
-    activeTask = task.id;
+function makeTaskActive(task) {      
+  activeTask = task.id;
+  console.log(activeTask)
 
-    console.log(activeTask);
-    let newTask = document.getElementById(activeTask.toString());
-    newTask.classList.toggle('active');
-    console.log(newTask);
+  if (task.activeStatus === false) {
+    task.activeStatus = true;
+    tasks = tasks.map(task => {
+      if (task.id !== activeTask) {
+        task.activeStatus = false;
+        return task;
+      } 
+      return task;
+    })
+  } else {
+    task.activeStatus = false;
+  }
+  console.log(tasks);
+  renderApp();
+}
 
-    red.onclick = () => {
-        if (task.redStatus === false) {
-            makeTaskRed();
-            renderApp();
-        } else {
-            makeTaskDafault();
-            renderApp();
-        }
-    }
+function highlightingTask(task) {  
 
-    function makeTaskRed() {
-        tasks = tasks.map(task => {
-            if (task.id === activeTask) {
-                task.redStatus = true;
-                task.yellowStatus = false;
-                task.greenStatus = false;
-                activeTask = null;
-                return task;
-            } else {
-                return task;
-            }
-        })
-        console.log(tasks)
+  console.log(task)
+  red.onclick = () => {
+    if (task.redStatus === false) {
+      makeTaskRed();
+      renderApp();
+    } else {
+      makeTaskDafault();
+      renderApp();
     }
+  }
 
-    yellow.onclick = () => {
-        if (task.yellowStatus === false) {
-            makeTaskYellow();
-            renderApp();
-        } else {
-            makeTaskDafault();
-            renderApp();
-        }
-    }
-    function makeTaskYellow() {
-        tasks = tasks.map(task => {
-            if (task.id === activeTask) {
-                task.yellowStatus = true;
-                task.redStatus = false;
-                task.greenStatus = false;
-                activeTask = null;
-                return task;
-            } else {
-                return task;
-            }
-        })
-        console.log(tasks)
-    }
-    green.onclick = () => {
-        if (task.greenStatus === false) {
-            makeTaskGreen();
-            renderApp();
-        } else {
-            makeTaskDafault();
-            renderApp();
-        }
-    }
-    function makeTaskGreen() {
-        tasks = tasks.map(task => {
-            if (task.id === activeTask) {
-                task.greenStatus = true;
-                task.redStatus = false;
-                task.yellowStatus = false;
-                activeTask = null;
-                return task;
-            } else {
-                return task;
-            }
-        })
-        console.log(tasks)
-    }
+  function makeTaskRed() {
+    tasks = tasks.map(task => {
+      if (task.activeStatus === true) {
+        task.redStatus = true;
+        task.yellowStatus = false;
+        task.greenStatus = false;
+        task.activeStatus = false;
+        return task;
+      } else {
+        return task;
+      }
+    })
+    console.log(tasks)
+  }
 
-    function makeTaskDafault() {
-        tasks = tasks.map(task => {
-            if (task.id === activeTask) {
-                task.redStatus = false;
-                task.yellowStatus = false;
-                task.greenStatus = false;
-                activeTask = null;
-                return task;
-            } else {
-                return task;
-            }
-        })
-        console.log(tasks)
+  yellow.onclick = () => {
+    if (task.yellowStatus === false) {
+      makeTaskYellow();
+      renderApp();
+    } else {
+      makeTaskDafault();
+      renderApp();
     }
+  }
+  function makeTaskYellow() {
+    tasks = tasks.map(task => {
+      if (task.activeStatus === true) {
+        task.yellowStatus = true;
+        task.redStatus = false;
+        task.greenStatus = false;
+        task.activeStatus = false;
+        return task;
+      } else {
+        return task;
+      }
+    })
+    console.log(tasks)
+  }
+  green.onclick = () => {
+    if (task.greenStatus === false) {
+      makeTaskGreen();
+      renderApp();
+    } else {
+      makeTaskDafault();
+      renderApp();
+    }
+  }
+  function makeTaskGreen() {
+    tasks = tasks.map(task => {
+      if (task.activeStatus === true) {
+        task.greenStatus = true;
+        task.redStatus = false;
+        task.yellowStatus = false;
+        task.activeStatus = false;
+        return task;
+      } else {
+        return task;
+      }
+    })
+    console.log(tasks)
+  }
+
+  function makeTaskDafault() {
+    tasks = tasks.map(task => {
+      if (task.activeStatus === true) {
+        task.redStatus = false;
+        task.yellowStatus = false;
+        task.greenStatus = false;
+        task.activeStatus = false;
+        return task;
+      } else {
+        return task;
+      }
+    })
+    console.log(tasks)
+  }
 }
 
 function deleteOneTask(task) {
-    let id = task.id;
-    Swal.fire({
-        title: 'Точно удалить задачу?',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Удалить!',
-        cancelButtonText: 'Нет',
-    }).then((result) => {
-        if (result.isConfirmed) {
-            tasks = tasks.filter(task => task.id !== id);
-            renderApp();
-            Swal.fire(
-                'Задача удалена!',
-                '',
-                'success'
-            )
-        }
-    })
+  let id = task.id;
+  Swal.fire({
+    title: 'Точно удалить задачу?',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Удалить!',
+    cancelButtonText: 'Нет',
+  }).then((result) => {
+    if (result.isConfirmed) {
+      tasks = tasks.filter(task => task.id !== id);
+      renderApp();
+      Swal.fire(
+        'Задача удалена!',
+        '',
+        'success'
+      )
+    }
+  })
 }
 
