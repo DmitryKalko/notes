abb1fac2c6adc2aac264b7206a3c68237e88fac3
Dmitry Kalko <master-denn@mail.ru>
2020-11-09 01:16:30 +0300

    add three new themes

--- src/leftMenu.js
+++ src/leftMenu.js
@@ -4,6 +4,7 @@ const leftMenu = document.querySelector('.left-menu');
 const businesThemeIcon = document.querySelector('.icon-briefcase');
 const nightThemeIcon = document.querySelector('.icon-moon-fill');
 const fireThemeIcon = document.querySelector('.icon-fire');
+const waterThemeIcon = document.querySelector('.icon-droplet');
 const mainBlock = document.querySelector('.main-block');
 
 //--- появление / исчезновение левого меню ---
@@ -63,6 +64,11 @@ fireThemeIcon.onclick = () => {
     window.localStorage.setItem('currentTheme', JSON.stringify(currentTheme));
     changeTheme();
 }
+waterThemeIcon.onclick = () => {
+    currentTheme = 'water';
+    window.localStorage.setItem('currentTheme', JSON.stringify(currentTheme));
+    changeTheme();
+}
 
 function changeTheme() {
     console.log(localStorage);
@@ -73,26 +79,97 @@ function changeTheme() {
         setNightTheme();
     } else if (currentTheme === 'fire') {
         setFireTheme();
+    } else if (currentTheme === 'water') {
+        setWaterTheme();
     }
 }
 
 function setBusinesTheme() {
+    //removeStars();
     mainBlock.classList.add('busines');
     leftMenu.classList.add('busines');
+    mainBlock.classList.remove('night');
+    leftMenu.classList.remove('night');
+    mainBlock.classList.remove('fire');
+    leftMenu.classList.remove('fire');
+    mainBlock.classList.remove('water');
+    leftMenu.classList.remove('water');
     businesThemeIcon.classList.add('briefcase-active');
     nightThemeIcon.classList.remove('moon-active');
     fireThemeIcon.classList.remove('fire-active');
+    waterThemeIcon.classList.remove('water-active');
     console.log('busines');
 }
 function setNightTheme() {
+    mainBlock.classList.add('night');
+    leftMenu.classList.add('night');
+    mainBlock.classList.remove('fire');
+    leftMenu.classList.remove('fire');
+    mainBlock.classList.remove('busines');
+    leftMenu.classList.remove('busines');
+    mainBlock.classList.remove('water');
+    leftMenu.classList.remove('water');
+    //stars();
     nightThemeIcon.classList.add('moon-active');
     businesThemeIcon.classList.remove('briefcase-active');
     fireThemeIcon.classList.remove('fire-active');
+    waterThemeIcon.classList.remove('water-active');
     console.log('night');
 }
 function setFireTheme() {
+    mainBlock.classList.add('fire');
+    leftMenu.classList.add('fire');
+    mainBlock.classList.remove('busines');
+    leftMenu.classList.remove('busines');
+    mainBlock.classList.remove('night');
+    leftMenu.classList.remove('night');
+    mainBlock.classList.remove('water');
+    leftMenu.classList.remove('water');
     fireThemeIcon.classList.add('fire-active');
     businesThemeIcon.classList.remove('briefcase-active');
     nightThemeIcon.classList.remove('moon-active');
+    waterThemeIcon.classList.remove('water-active');
     console.log('fire');
 }
+function setWaterTheme() {
+    mainBlock.classList.add('water');
+    leftMenu.classList.add('water');
+    mainBlock.classList.remove('busines');
+    leftMenu.classList.remove('busines');
+    mainBlock.classList.remove('night');
+    leftMenu.classList.remove('night');
+    mainBlock.classList.remove('fire');
+    leftMenu.classList.remove('fire');
+    waterThemeIcon.classList.add('water-active');
+    businesThemeIcon.classList.remove('briefcase-active');
+    nightThemeIcon.classList.remove('moon-active');
+    fireThemeIcon.classList.remove('fire-active');
+    console.log('water');
+}
+
+// function stars() {
+//     let count = 300;
+//     let i = 0;
+//     while(i < count) {
+//         let star = document.createElement('i');
+//         let x = Math.floor(Math.random() * window.innerWidth);
+//         let y = Math.floor(Math.random() * window.innerHeight);
+//         let size = Math.random() * 2;
+
+//         star.style.left = x + 'px';
+//         star.style.top = y + 'px';
+//         star.style.width = 1 + size + 'px';
+//         star.style.height = 1 + size + 'px';
+
+//         mainBlock.append(star);
+//         i++;
+
+//     }
+// }
+// function removeStars() { 
+//     let stars = document.getElementsByTagName('i');
+    
+//     while (stars.length > 0) {
+//         stars[0].parentNode.removeChild(stars[0]);
+//     }
+// }
